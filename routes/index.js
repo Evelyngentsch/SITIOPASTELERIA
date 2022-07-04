858bb329ecef759eb8462956f211dec3a29d538c
Evelyn Gentsch <eve.gentsch@gmail.com>
2022-07-03 21:52:23 -0300

    entrega

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,43 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.telefono;
+  var mensaje = req.body.mensaje;
+  //console.log(req.body)
+
+  var obj = {
+    to: 'eve.gentsch@gmail.com',
+    subject: 'CONTACTO WEB',
+    html: nombre + " " + apellido + " se contacto a travez de la web y quiere mas información a este correo : " + email + ". <br> Además, hizo este comentario : " + mensaje + ". <br> Su teléfono es : " + telefono
+  } //cierra obj
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+
+    }
+  }) //cierra transporter
+
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+
+}); //cierra peticion del post
+
 
 module.exports = router;
